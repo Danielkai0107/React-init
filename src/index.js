d9964567c1e4831edda346f7bc02f8a2fac70cbd
Daniel <danielwzk0107@gmail.com>
2022-09-15 15:43:32 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './styles/globals.scss';
+import { AppProvider } from './func/context';
 import HomePage from './pages/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <AppProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   </React.StrictMode>,
 );
